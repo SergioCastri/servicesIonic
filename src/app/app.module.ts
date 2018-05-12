dce05c3cb98a15c595e1e2c79ffed7b074b1dc4a
sergiocastri <scastrillon347@gmail.com>
2018-05-12 10:59:11 -0500

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { UsersProvider } from '../providers/usuarios/usuarios';
+import { PhotosProvider } from '../providers/fotos/fotos';
+import { Segment1PageModule } from '../pages/segment/segment.module';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage
+  ],
+  imports: [
+    BrowserModule,
+    Segment1PageModule,
+    HttpClientModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    UsersProvider,
+    PhotosProvider
+  ]
+})
+export class AppModule {}
